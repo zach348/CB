e1dcc808991c3e7d7c188e6940b378566aa480b3
Zack Cutler <zachlcutler@gmail.com>
2019-05-27 17:32:21 -0400

    created BallGroup struct; created Ball class

---
+++ NinjaAttackStarter/Classes/BallGroup.swift
@@ -0,0 +1,53 @@
+/// Copyright (c) 2019 Razeware LLC
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import Foundation
+import SpriteKit
+
+struct BallGroup {
+  var members = [Ball]()
+  var gameScene: GameScene
+  
+  init(gameScene: GameScene){
+    self.gameScene = gameScene
+  }
+  
+  mutating func createBall(){
+    let ball = Ball(imageName: "monster")
+    self.members.append(ball)
+    ball.position = CGPoint(x: self.gameScene.size.width/2, y: self.gameScene.size.height/2)
+  }
+  
+  func addMemberstoScene(){
+    for ball in self.members {
+      self.gameScene.addChild(ball)
+    }
+  }
+
+}
+
--- NinjaAttackStarter/NinjaAttack/GameScene.swift
+++ NinjaAttackStarter/NinjaAttack/GameScene.swift
@@ -28,6 +28,39 @@
 
 import SpriteKit
 
+func +(left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func -(left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func *(point: CGPoint, scalar: CGFloat) -> CGPoint {
+  return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func /(point: CGPoint, scalar: CGFloat) -> CGPoint {
+  return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+#if !(arch(x86_64) || arch(arm64))
+func sqrt(a: CGFloat) -> CGFloat {
+  return CGFloat(sqrtf(Float(a)))
+}
+#endif
+
+extension CGPoint {
+  func length() -> CGFloat {
+    return sqrt(x*x + y*y)
+  }
+  
+  func normalized() -> CGPoint {
+    return self / length()
+  }
+}
+
+
 class GameScene: SKScene {
   // 1
   let player = SKSpriteNode(imageNamed: "player")
@@ -36,9 +69,7 @@ class GameScene: SKScene {
     // 2
     backgroundColor = SKColor.white
     // 3
-    player.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
-    // 4
-    addChild(player)
+    
     
     func random() -> CGFloat {
       return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
@@ -48,35 +79,45 @@ class GameScene: SKScene {
       return random() * (max - min) + min
     }
     
-    func addMonster() {
-      
-      // Create sprite
-      let monster = SKSpriteNode(imageNamed: "monster")
-      
-      // Determine where to spawn the monster along the Y axis
-      let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
-      
-      // Position the monster slightly off-screen along the right edge,
-      // and along a random position along the Y axis as calculated above
-      monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
-      
-      // Add the monster to the scene
-      addChild(monster)
-      
-      // Determine speed of the monster
-      let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
-      
-      // Create the actions
-      let actionMove = SKAction.move(to: CGPoint(x: -monster.size.width/2, y: actualY),
-                                     duration: TimeInterval(actualDuration))
-      let actionMoveDone = SKAction.removeFromParent()
-      monster.run(SKAction.sequence([actionMove, actionMoveDone]))
-    }
-    run(SKAction.repeatForever(
-      SKAction.sequence([
-        SKAction.run(addMonster),
-        SKAction.wait(forDuration: 1.0)
-        ])
-    ))
+    var ballGroup = BallGroup(gameScene: self)
+    ballGroup.createBall()
+    ballGroup.addMemberstoScene()
+    
+    
+//    func addMonster() {
+//
+//      // Create sprite
+//      let monster = SKSpriteNode(imageNamed: "monster")
+//
+//      // Determine where to spawn the monster along the Y axis
+//      let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
+//
+//      // Position the monster slightly off-screen along the right edge,
+//      // and along a random position along the Y axis as calculated above
+//      monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
+//
+//      // Add the monster to the scene
+//      addChild(monster)
+//
+//      // Determine speed of the monster
+//      let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+//
+//      // Create the actions
+//      let actionMove = SKAction.move(to: CGPoint(x: -monster.size.width/2, y: actualY),
+//                                     duration: TimeInterval(actualDuration))
+//      let actionMoveDone = SKAction.removeFromParent()
+//      monster.run(SKAction.sequence([actionMove, actionMoveDone]))
+//    }
+    
+
+    
+    
+    
+//    run(SKAction.repeatForever(
+//      SKAction.sequence([
+//        SKAction.run(addMonster),
+//        SKAction.wait(forDuration: 1.0)
+//        ])
+//    ))
   }
 }
