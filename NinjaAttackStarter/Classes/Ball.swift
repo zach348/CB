542dd39ac2d6b0eb4f4272e1dc0d087ce686416c
Zack Cutler <zachlcutler@gmail.com>
2019-07-09 19:51:36 -0400

    pcm

--- NinjaAttackStarter/Classes/Ball.swift
+++ NinjaAttackStarter/Classes/Ball.swift
@@ -307,16 +307,15 @@ class Ball: SKSpriteNode {
     }
   }
     
-  func flickerOutTarget(duration:TimeInterval = 1.0){
+  func flickerOutTarget(duration:TimeInterval = 0.75){
     let off = SKAction.setTexture(Game.currentTrackSettings.distractorTexture)
     let on = SKAction.setTexture(Game.currentTrackSettings.targetTexture)
     let waitAction = SKAction.wait(forDuration: duration)
     if duration < 0.01 {
       self.run(off)
     }else{
-      let newDuration = duration * Double.random(min: 0.6, max: 0.7)
+      let newDuration = duration * Double.random(min: 0.8, max: 0.85)
       let recursiveCall = SKAction.run {
-        print("recursion", duration)
         self.flickerOutTarget(duration:newDuration)
       }
       self.run(SKAction.sequence([off,waitAction,on,waitAction]), completion: { self.run(recursiveCall)})
--- NinjaAttackStarter/Classes/Game.swift
+++ NinjaAttackStarter/Classes/Game.swift
@@ -5,7 +5,7 @@ import SpriteKit
 class Game {
   
   static var settingsArr:[Settings] = [
-    Settings(phase: 1, phaseDuration: 50, pauseDelay: 10, pauseError: 2, pauseDuration: 1.5, frequency: 18, toneFile: "tone200hz.wav", targetMeanSpeed: 650, targetSpeedSD: 375, shiftDelay: 4, shiftError: 2, numTargets: 1, targetTexture: "sphere-darkGray", distractorTexture: "sphere-darkGray", flashTexture: "sphere-red", alpha: 1),
+    Settings(phase: 1, phaseDuration: 50, pauseDelay: 10, pauseError: 2, pauseDuration: 1.5, frequency: 18, toneFile: "tone200hz.wav", targetMeanSpeed: 550, targetSpeedSD: 325, shiftDelay: 4, shiftError: 2, numTargets: 1, targetTexture: "sphere-darkGray", distractorTexture: "sphere-darkGray", flashTexture: "sphere-red", alpha: 1),
     Settings(phase: 2, phaseDuration: 70, pauseDelay: 15, pauseError: 4, pauseDuration: 2.5, frequency: 14, toneFile: "tone185hz.wav", targetMeanSpeed: 500, targetSpeedSD: 275, shiftDelay: 7, shiftError: 4, numTargets: 2, targetTexture: "sphere-blue1", distractorTexture: "sphere-blue2", flashTexture: "sphere-red", alpha: 1),
     Settings(phase: 3, phaseDuration: 80, pauseDelay: 22, pauseError: 6, pauseDuration: 4, frequency: 10, toneFile: "tone170hz.wav", targetMeanSpeed: 375, targetSpeedSD: 175, shiftDelay: 10, shiftError: 6, numTargets: 3, targetTexture: "sphere-purple", distractorTexture: "sphere-magenta", flashTexture: "sphere-red", alpha: 1),
     Settings(phase: 4, phaseDuration: 100, pauseDelay: 30, pauseError: 6, pauseDuration: 5, frequency: 8, toneFile: "tone155hz.wav", targetMeanSpeed: 275, targetSpeedSD: 75, shiftDelay: 25, shiftError: 8, numTargets: 4, targetTexture: "sphere-darkTurquoise", distractorTexture: "sphere-green", flashTexture: "sphere-white", alpha: 1),
@@ -13,11 +13,11 @@ class Game {
     //messing with duration for dev
     Settings(phase: 6, phaseDuration: 5, pauseDelay: 40, pauseError: 10, pauseDuration: 7, frequency: 5, toneFile: "tone140hz.wav", targetMeanSpeed: 75, targetSpeedSD: 0, shiftDelay: 50, shiftError: 15, numTargets: 6, targetTexture: "sphere-orange", distractorTexture: "sphere-black", flashTexture: "sphere-white", alpha: 1),
     //Final settings is a dummy phase... used only to trigger the phase property-based trigger for transition into resp
-    Settings(phase: 7, phaseDuration: 180, pauseDelay: 40, pauseError: 10, pauseDuration: 7, frequency: 5, toneFile: "tone140hz.wav", targetMeanSpeed: 0, targetSpeedSD: 0, shiftDelay: 50, shiftError: 15, numTargets: 6, targetTexture: "sphere-orange", distractorTexture: "sphere-black", flashTexture: "sphere-orange", alpha: 1)
+    Settings(phase: 7, phaseDuration: 900, pauseDelay: 40, pauseError: 10, pauseDuration: 7, frequency: 5, toneFile: "tone140hz.wav", targetMeanSpeed: 0, targetSpeedSD: 0, shiftDelay: 50, shiftError: 15, numTargets: 6, targetTexture: "sphere-orange", distractorTexture: "sphere-black", flashTexture: "sphere-orange", alpha: 1)
   ]
   
   static var respSettingsArr:[RespSettings] = [
-    RespSettings(phase: 7, frequency: 5, inDuration: 4, inWait: 2, outDuration: 3, outWait: 1.5, moveToCenterDuration: 6, moveCenterWait: 2)
+    RespSettings(phase: 7, frequency: 5, inDuration: 3.5, inWait: 1.5, outDuration: 5, outWait: 3, moveToCenterDuration: 6, moveCenterWait: 2)
   ]
   
   //TESTING
@@ -25,29 +25,25 @@ class Game {
     didSet{
       //logic for transition into resp
       if respTransition {
-        if let timer = currentGame.timer {
+        if let timer = currentGame.timer, let world = currentGame.world {
           //stop targetShift and pause timers
           timer.members.forEach({ loop in
             if loop != "frequencyLoopTimer" && loop != "gameTimer"  && loop != "movementTimer" {timer.stopTimer(timerID: loop)}
           })
           //implement flicker effect
-          let targetAssignment = SKAction.run({
-            Ball.assignRandomTargets()
-          })
-          let wait = SKAction.wait(forDuration: 2)
-          let flickerAction = SKAction.run {
-            Ball.getTargets().forEach({ball in ball.flickerOutTarget()})
-          }
-          if let world = currentGame.world {world.run(SKAction.sequence([targetAssignment,wait,flickerAction]))}
-         
+          Ball.getTargets().forEach({ball in ball.flickerOutTarget()})
           //stop master movement timer prior to calling circleMovementTimer
-//          timer.circleMovementTimer()
+          let wait = SKAction.wait(forDuration: 15)
+          let stopMovementTimer = SKAction.run({ timer.stopTimer(timerID: "movementTimer")})
+          let circleAction = SKAction.run({ timer.circleMovementTimer()})
+          world.run(SKAction.sequence([wait,stopMovementTimer,wait,circleAction]))
         }
       }
     }
   }
+  ///STARTING POINTS
   static var currentRespSettings:RespSettings = respSettingsArr[0]
-  static var currentTrackSettings:Settings = settingsArr[5] {
+  static var currentTrackSettings:Settings = settingsArr[0] {
     didSet {
       if let timer = currentGame.timer {
         print(currentGame.timer!.members)
@@ -79,9 +75,12 @@ class Game {
   
   class func advancePhase(){
     if let index = self.settingsArr.firstIndex(where: { setting in setting.phase == self.currentTrackSettings.phase + 1 }), let timer = currentGame.timer {
-      if index < self.settingsArr.count {
+      if !self.respTransition && index < self.settingsArr.count {
         self.currentTrackSettings = self.settingsArr[index]
         timer.lastPhaseShiftTime = timer.elapsedTime
+      }else{
+        //do resp stuff here
+        
       }
       //TESTING
     }
