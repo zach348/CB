0b4580d3b51730d49825138726c1b4c3473914c5
Zack Cutler <zachlcutler@gmail.com>
2019-09-06 19:50:42 -0400

    successfully saved test data to firestore

---
+++ NinjaAttackStarter/Program Files/DataStore.swift
@@ -0,0 +1,83 @@
+
+
+import Foundation
+import SpriteKit
+import Firebase
+
+struct DataStore {
+  static var records = [DataStore]()
+  
+  static func addRecord(){
+    if let timer = currentGame.timer, let scene = currentGame.gameScene {
+      timer.stopTimer(timerID: "dataTimer")
+      self.records.append(self.init(bpm: currentGame.bpm))
+      let dataTimer = SKAction.run {
+        timer.dataTimer()
+      }
+      scene.run(dataTimer)
+    }
+  }
+  
+  static func saveGame(){
+    let docRef = Firestore.firestore().document("sample_games/test_data")
+    let testString = "test data"
+//    let dataToSave: [String: Any] = [
+//      
+//    ]
+    
+    
+    
+    docRef.setData(dataToSave) { (error) in
+      if let error = error {
+        print("error: \(error.localizedDescription)")
+      } else {
+        print("Data has been saved")
+      }
+    }
+  }
+  
+  let elapsedTime:Double
+  let isResponding:Bool
+  let bpm:Int
+  let meanSpeed:CGFloat
+  let speedSD:CGFloat
+  //from Settings
+  let phase:Int
+  let requiredStreak:Int
+  let pauseDelay:Double
+  let pauseError:Double
+  let pauseDuration:Double
+  let frequency:Double
+  let toneFile:String
+  let targetMeanSpeed:CGFloat
+  let targetSpeedSD:CGFloat
+  let shiftDelay:Double
+  let shiftError:Double
+  let numTargets:Int
+  let ballCount:Int
+  let targetTexture:String
+  let distractorTexture:String
+  
+  init(bpm:Int){
+    self.elapsedTime = currentGame.timer!.elapsedTime
+    self.isResponding = currentGame.isPaused
+    self.bpm = bpm
+    self.meanSpeed = Ball.mean()
+    self.speedSD = Ball.standardDev()
+    self.phase = Game.currentTrackSettings.phase
+    self.requiredStreak = Game.currentTrackSettings.requiredStreak
+    self.pauseDelay = Game.currentTrackSettings.pauseDelay
+    self.pauseError = Game.currentTrackSettings.pauseError
+    self.pauseDuration = Game.currentTrackSettings.pauseDuration
+    self.frequency = Game.currentTrackSettings.frequency
+    self.toneFile = Game.currentTrackSettings.toneFile
+    self.targetMeanSpeed = Game.currentTrackSettings.targetMeanSpeed
+    self.targetSpeedSD = Game.currentTrackSettings.targetSpeedSD
+    self.shiftDelay = Game.currentTrackSettings.shiftDelay
+    self.shiftError = Game.currentTrackSettings.shiftError
+    self.numTargets = Game.currentTrackSettings.numTargets
+    self.ballCount = Ball.members.count
+    self.targetTexture = Game.currentTrackSettings.targetTexture.description
+    self.distractorTexture = Game.currentTrackSettings.distractorTexture.description
+  }
+}
