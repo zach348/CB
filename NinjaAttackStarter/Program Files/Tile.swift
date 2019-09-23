bdda1fd728d7066fb80823ae6f985a01d1296dff
Zack Cutler <zachlcutler@gmail.com>
2019-09-23 14:37:30 -0400

    successfully integrated changes, nonfunctional button labels and all, into master without merging startScreen feature branch; what a fucking mess

--- NinjaAttackStarter/Program Files/Tile.swift
+++ NinjaAttackStarter/Program Files/Tile.swift
@@ -1,37 +1,11 @@
-/// Copyright (c) 2019 Razeware LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
+
 
 import Foundation
 import SpriteKit
 
 class Tile: SKShapeNode {
   static var members = [Tile]()
-
+  
   class func createTile(position:CGPoint, tileName:String){
     let tile = Tile()
     tile.position = position
@@ -59,9 +33,9 @@ class Tile: SKShapeNode {
     }
   }
   
- override init(){
+  override init(){
     super.init()
-//    super.init(rectOf: CGSize(width: currentGame.gameScene!.frame.width/5, height: currentGame.gameScene!.frame.height/3))
+    //    super.init(rectOf: CGSize(width: currentGame.gameScene!.frame.width/5, height: currentGame.gameScene!.frame.height/3))
     self.path = CGPath(rect: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: currentGame.gameScene!.frame.width/5, height: currentGame.gameScene!.frame.height/3)), transform: nil)
     self.zPosition = -1
     self.fillColor = SKColor.white
--- NinjaAttackStarter/Program Files/Timer.swift
+++ NinjaAttackStarter/Program Files/Timer.swift
@@ -8,11 +8,14 @@ class Timer {
     didSet {
       if Ball.blinkFlags.isEmpty {
         self.remainingInPhase = Game.currentTrackSettings.phaseDuration - (self.elapsedTime - self.lastPhaseShiftTime)
-        if self.remainingInPhase <  0 && !currentGame.isPaused && currentGame.streakAchieved { Game.advancePhase() }
+        //play mode
+        if (currentGame.isPaused && currentGame.streakAchieved) { Game.advancePhase() }
+        //demo mode
+//      if (!currentGame.isPaused && self.elapsedTime - self.lastPhaseShiftTime > 45) { Game.advancePhase()}
       }
     }
   }
-  var lastPhaseShiftTime:Double
+  var lastPhaseShiftTime:Double = 0
   var remainingInPhase:Double
   init(){
     self.members = []
@@ -26,13 +29,13 @@ class Timer {
     let count = SKAction.run {
       self.elapsedTime += 0.05
     }
-  //Master Game block kept at top level/ on gamescene instance
+    //Master Game block kept at top level/ on gamescene instance
     if let scene = currentGame.gameScene {
       self.members.append("gameTimer")
       scene.run(SKAction.repeatForever(SKAction.sequence([wait,count])), withKey: "gameTimer")
     }
   }
-
+  
   func movementTimer(){
     if let worldTimer = currentGame.worldTimer {
       let wait = SKAction.wait(forDuration: 0.025)
@@ -169,7 +172,7 @@ class Timer {
     }
   }
   
-  func dataTimer(){
+  func dataTimer(saveGame:Bool = false){
     if let scene = currentGame.gameScene {
       self.stopTimer(timerID: "dataTimer")
       let wait = SKAction.wait(forDuration: 0.5)
